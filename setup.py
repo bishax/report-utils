c1b8a4aeccf3577c348d7d82e63edca7625a9b56
bishax <alex.bishop@nesta.org.uk>
2021-03-03 17:58:47 +0000

    Setup Nesta Data Science cookiecutter (#7)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='report_utils',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploratory project for defining a technical report writing workflow.',
+    author='Alex Bishop',
+    license='MIT',
+)
