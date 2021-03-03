b3084ebe61663409338c8bcb04e96f4de229295c
bishax <alex.bishop@nesta.org.uk>
2021-03-03 17:06:12 +0000

    Update scripts to make report and html

--- bin/make_report.sh
+++ bin/make_report.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-cd $(dirname $0)/../reports/technical_report
-ln -s ../../figures/scotland . || echo ""
-ln -s ../../figures/notice_length.png . || echo ""
-pandoc -s 0_exec_summary.md\
- 1_introduction.md\
- 2_literature.md\
- 3_methodology.md\
- 4_results.md\
- 5_conclusions.md\
+cd $(dirname $0)/../output/
+pandoc -s report.md\
  --metadata-file latex_metadata.yaml\
  -f markdown\
  -o report.tex\
  --bibliography 'technicalreport.bib'\
- --filter ../../bin/altair_pandoc_filter.py\
- --metadata figure_path="../../figures"\
+ --filter ../bin/pandoc_altair_filter.py\
  --natbib -F pandoc-crossref\
- --resource-path="../../figures/.:."\
- --variable fontfamily=arev\
+ --metadata figure_path="figures"\
  --variable urlcolor=blue
 pdflatex report.tex
 bibtex report || echo "Bibtex unsuccessful!"
 pdflatex report.tex
 pdflatex report.tex
-\rm scotland notice_length.png
